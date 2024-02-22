9ba0b9c08a61791bc57813e6f3ed480c5c26f065
Anthony Bryson <antbryson17@gmail.com>
2024-02-22 15:08:40 -0800

    added mtmct

---
+++ AICITY2022_Track1_TAG/detector/yolov5/gen_det.sh
@@ -0,0 +1,20 @@
+seqs=(c041 c042)
+gpu_id=0
+for seq in ${seqs[@]}
+do
+    CUDA_VISIBLE_DEVICES=${gpu_id} python detect2img.py --name ${seq} --weights yolov5x.pt --conf 0.1 --agnostic --save-conf --img-size 640 --classes 2 5 7 --exist-ok --cfg_file $1& gpu_id=$(($gpu_id+1))
+done
+
+seqs=(c043 c044)
+gpu_id=0
+for seq in ${seqs[@]}
+do
+    CUDA_VISIBLE_DEVICES=${gpu_id} python detect2img.py --name ${seq} --weights yolov5x.pt --conf 0.1 --agnostic --save-conf --img-size 640 --classes 2 5 7 --exist-ok --cfg_file $1& gpu_id=$(($gpu_id+1))
+done
+
+seqs=(c045 c046)
+gpu_id=0
+for seq in ${seqs[@]}
+do
+    CUDA_VISIBLE_DEVICES=${gpu_id} python detect2img.py --name ${seq} --weights yolov5x.pt --conf 0.1 --agnostic --save-conf --img-size 640 --classes 2 5 7 --exist-ok --cfg_file $1& gpu_id=$(($gpu_id+1))
+done
