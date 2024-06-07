284c65dad59822e6fa60549206f6beff45601c0a
Anthony Bryson <antbryson17@gmail.com>
2024-06-06 17:38:24 -0700

    updated scripts

--- AICITY2022_Track1_TAG/MCMVT.sh
+++ AICITY2022_Track1_TAG/MCMVT.sh
@@ -1,12 +1,13 @@
 MCMT_CONFIG_FILE="aic.yml"
 
+### Feature Extraction ###
 cd ./reid_bidir/
 python extract_image_feat.py "aic_reid1.yml"
 python extract_image_feat.py "aic_reid2.yml"
 python extract_image_feat.py "aic_reid3.yml"
 python merge_reid_feat.py ${MCMT_CONFIG_FILE}
 
-# #### MOT ####
+### MOT ###
 cd ../tracker/MOTBaseline
 sh run_aic.sh ${MCMT_CONFIG_FILE}
 cd ../../reid_bidir/reid-matching/tools
@@ -20,7 +21,3 @@ python assign_movements.py
 cd ../AICITY2022_Track1_TAG/reid_bidir/reid-matching/tools
 python sub_cluster.py ${MCMT_CONFIG_FILE}
 python gen_res.py ${MCMT_CONFIG_FILE}
-
-# python interpolation.py ${MCMT_CONFIG_FILE}
-
-
